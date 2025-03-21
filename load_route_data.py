20ddb49da790263f059d314b242a14d08e3b4baf
Max Ingram <72043457+Max98620@users.noreply.github.com>
2025-03-21 18:39:03 +0000

    fix

--- load_route_data.py
+++ load_route_data.py
@@ -6,6 +6,6 @@ def load_route_data(filename):
         print(err)
     route_data = {}
     for rows in range(1,len(route)):
-        column = route[i].split(',')
+        column = route[rows].split(',')
         route_data.update({column[0]:column[3]})
     return route_data
