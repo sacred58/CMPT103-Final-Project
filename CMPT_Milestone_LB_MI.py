88d8597f24f0911610f455c48b8599f5be7c4009
sacred58 <78624958+sacred58@users.noreply.github.com>
2025-03-21 21:20:02 +0000

    changed print statements for pickle loading and saving

--- CMPT_Milestone_LB_MI.py
+++ CMPT_Milestone_LB_MI.py
@@ -259,7 +259,7 @@ Edmonton Transit System
             if filename == '':
                 filename = pickle_file_path
             save_data(routes,shapes,filename)
-            print(f"Data from {filename} loaded")
+            print(f"Data structures successfully written to {filename}")
             continue
         elif user_input == '8':
             # Load route_names, routes, shapes from the aforementioned pickle
@@ -271,7 +271,7 @@ Edmonton Transit System
             except:
                 continue
 
-            print(f"Data structures successfully written to {filename}")
+            print(f'Data from {filename} loaded')
             
             continue
         elif user_input == '9':
