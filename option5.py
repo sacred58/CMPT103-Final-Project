9dce524070ebdb336023fbb974ecde8d4118f63c
sacred58 <78624958+sacred58@users.noreply.github.com>
2025-03-21 20:36:40 +0000

    building option 5

--- option5.py
+++ option5.py
@@ -1,3 +1,16 @@
-def search_shape_id(routes, shape_id):
+from CMPT_Milestone_LB_MI import load_shapes
 
-    return None
+def search_shape_id(shapes, shape_id):
+
+    try:
+        shape = shapes[shape_id]
+    except:
+        print("\t** NOT FOUND **")
+        return
+    
+    print(f'Shape ID coordinates for {shape_id} are:')
+    for coord in shape:
+        print(coord)
+
+shapes = load_shapes('data/shapes.txt')
+search_shape_id(shapes, '112-3-East')
