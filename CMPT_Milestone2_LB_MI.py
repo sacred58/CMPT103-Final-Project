1f309866dc879d5d8ac5474bdb98761596e89d01
sacred58 <78624958+sacred58@users.noreply.github.com>
2025-03-28 18:51:28 +0000

    added option3 functionality

--- CMPT_Milestone2_LB_MI.py
+++ CMPT_Milestone2_LB_MI.py
@@ -4,6 +4,7 @@
 #------------------------------------
 
 import pickle
+from datetime import date
 
 def load_route_names(filename):
     '''
@@ -190,6 +191,58 @@ def load_data(pickle_file_path):
         print(err)
         return
     return routes, shapes
+
+def load_disruptions(filename):
+    '''
+    purpose: Loads dispruption data from a CSV file for traffic disruptions
+    parameters:
+        - filename: path of the disruption data file
+    return:
+        - disruptions (date_reference, (lat, lon)): Set of tuples containing a reference to a date object and a nested tuple
+        with the lat and lon of the disruption
+    '''
+
+    months = {'jan':1, 'feb':2, 'mar':3, 'apr':4, 'may':5, 'jun':6, 'jul':7, 'aug':8, 'sep':9, 'oct':10, 'nov':11, 'dec':12}
+    
+    try:
+        with open(filename, 'r') as file:
+            file_lines = file.readlines()
+            print(f'Data from {filename} loaded')
+    except IOError as err:
+        print(err)
+        return
+    
+    disruptions = set()
+    
+    for lines in range(1, len(file_lines)):
+        current_line = file_lines[lines].split(',')
+
+        # Process date data
+        # Turn the month into an int
+        month_str, day = current_line[5].lower().split(' ')
+        month_str = month_str[1:]
+
+        if month_str in months.keys():
+            month_int = months[month_str]
+
+        day = int(day)
+
+        year = int(current_line[6][:-1])
+
+        d = date(year, month_int, day)
+
+        # Process lat, lon data
+        point = current_line[-1][7:-2]
+        point = point.split(' ')
+
+        point[0] = float(point[0])
+        point[1] = float(point[1])
+        point = tuple(point)
+
+        disruptions.add((d, point))
+
+    return disruptions
+
 def main():
     '''
     purpose: Runs a menu to select other functions to load and view transit data
@@ -200,9 +253,11 @@ def main():
     shapes_file_path = 'data/shapes.txt'
     trips_file_path = 'data/trips.txt'
     pickle_file_path = 'data/etsdata.p'
+    disruptions_file_path = 'data/traffic_disruptions.txt'
     route_names = None
     routes = None
     shapes = None
+    disruptions = None
 
     options = '''
 Edmonton Transit System
@@ -247,7 +302,13 @@ Edmonton Transit System
             shapes = load_shapes(filename)
             
         elif user_input == '3':
-            print("Option 3 reserved for Milestone#2")
+            filename = input('Enter a filename:').strip()
+
+            if filename == '':
+                filename = disruptions_file_path
+
+            disruptions = load_disruptions(filename)
+            
         elif user_input == '4':
             # Print shape ids for route from routes dictionary
             if routes == None:
