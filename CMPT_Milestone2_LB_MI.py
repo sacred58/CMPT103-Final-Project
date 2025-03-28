1328e008bbb4f7d0a1a16b4ddb101ca1529f3b38
Max Ingram <72043457+Max98620@users.noreply.github.com>
2025-03-28 19:03:04 +0000

    latlontoxy

--- CMPT_Milestone2_LB_MI.py
+++ CMPT_Milestone2_LB_MI.py
@@ -251,6 +251,25 @@ def load_disruptions(filename):
 
     return disruptions
 
+def lonlat_to_xy(win, lon, lat):
+    '''Written by Philip Mees for CMPT 103
+    Purpose: convert longitude/latitude locations to x/y pixel locations
+        This avoids the use of the setCoords, toWorld, and toScreen methods and graphics.py incompatibilities
+    Parameters:
+        win (GraphWin): the GraphWin object of the GUI
+        lon, lat (float): longitude and latitude to be converted
+    Returns: x, y (int): pixel location inside win'''
+
+    xlow, xhigh = -113.720049, -113.320418
+    ylow, yhigh = 53.657116, 53.393703
+
+    width, height = win.getWidth(), win.getHeight()
+
+    x = (lon - xlow) / (xhigh - xlow) * width
+    y = (lat - ylow) / (yhigh - ylow) * height
+
+    return int(x), int(y)
+
 def main():
     '''
     purpose: Runs a menu to select other functions to load and view transit data
