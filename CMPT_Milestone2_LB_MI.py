388796592bce086fcd01d7fcd50ee30443fc047b
sacred58 <78624958+sacred58@users.noreply.github.com>
2025-03-28 19:38:48 +0000

    fixed compatability issue with python 3.10

--- CMPT_Milestone2_LB_MI.py
+++ CMPT_Milestone2_LB_MI.py
@@ -110,7 +110,8 @@ def get_shapeid(routes):
     '''
     route = input("Enter route: ").strip()
     if route in routes:
-        print(f"Shape ids for route [{routes[route]['name'].strip('"')}]")
+        route_name = routes[route]["name"].strip('"')
+        print(f"Shape ids for route [{route_name}]")
         for item in routes[route]['shape_ids']:
             print('\t' + str(item))
     else:
